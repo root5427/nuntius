8639a4c527842107b2ff71a0186a57145b624f49
Devix Rootix <shukrik27@gmail.com>
2019-08-22 14:06:32 +0300

    Removed unused import

--- test/pages/ChatPageList_test.dart
+++ test/pages/ChatPageList_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nuntius/pages/ChatPage.dart';
-import 'package:nuntius/pages/ChatPageList.dart';
 
 void main() {
   const MaterialApp app = MaterialApp(
