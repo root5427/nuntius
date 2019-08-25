4dcf83a65cc4dcc15b2082f88769855ee28dc755
Devix Rootix <shukrik27@gmail.com>
2019-08-26 02:09:52 +0300

    Fixed some tests

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nuntius/main.dart';
-import 'package:nuntius/pages/ChatPageList.dart';
+import 'package:nuntius/pages/RegisterPage.dart';
 
 void main() {
   testWidgets('Main UI test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    expect(find.byType(ChatPageList), findsOneWidget);
+    expect(find.byType(RegisterPage), findsOneWidget);
   });
 }
--- test/widgets/ChatListWidget_test.dart
+++ test/widgets/ChatListWidget_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nuntius/main.dart';
+import 'package:nuntius/widgets/ChatListWidget.dart';
 
 void main() {
+  MaterialApp app = MaterialApp(
+    home: Scaffold(
+      body: ChatListWidget(),
+    ),
+  );
   testWidgets('ChatListWidget UI Test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-    expect(find.byType(ListView), findsOneWidget);
+    await tester.pumpWidget(app);
+    expect(find.byType(ChatListWidget), findsOneWidget);
   });
 }
